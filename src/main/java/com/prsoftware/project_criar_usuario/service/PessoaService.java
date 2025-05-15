deeda8981217d7c8e12701d5ca0c609aa673b4b3
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-05-14 22:29:08 -0300

    Atualizando o Nome da Classe

---
+++ src/main/java/com/prsoftware/project_criar_usuario/service/PessoaService.java
@@ -0,0 +1,28 @@
+package com.prsoftware.project_criar_usuario.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.prsoftware.project_criar_usuario.entity.Pessoa;
+import com.prsoftware.project_criar_usuario.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    //Criando a Classe Para mostrar Todas as Pessoas
+    public List<Pessoa> findAll(){
+        return pessoaRepository.findAll();
+    }
+
+
+    //Criando Classe para Salvar as Pessoas
+    public Pessoa save(Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
+    }
+
+}
