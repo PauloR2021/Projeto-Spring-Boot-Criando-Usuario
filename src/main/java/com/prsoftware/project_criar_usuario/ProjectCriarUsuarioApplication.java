abdf9e666803113f5950100117e360acd6636b70
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-05-14 22:07:12 -0300

    Primeiro Commit

---
+++ src/main/java/com/prsoftware/project_criar_usuario/ProjectCriarUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.prsoftware.project_criar_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCriarUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCriarUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prsoftware/project_criar_usuario/ProjectCriarUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prsoftware.project_criar_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCriarUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
