bb91079b3323861e5996e73994e79cb6d0694a61
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-05-14 22:28:32 -0300

    Criando a Classe Controller aonde vai  chamar as rotas API

---
+++ src/main/java/com/prsoftware/project_criar_usuario/controller/PessoaController.java
@@ -0,0 +1,38 @@
+package com.prsoftware.project_criar_usuario.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.prsoftware.project_criar_usuario.entity.Pessoa;
+import com.prsoftware.project_criar_usuario.service.PessoaService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller //Iniciando a Classe Controller
+public class PessoaController {
+    
+    //Intanciando a Classe Service aonde está a Regra de Negocio do Projeto
+    @Autowired
+    private PessoaService pessoaService;
+
+    //Criando a API para listar as Pessoas Criadas
+    @GetMapping("/pessoas")
+    public String listarPessoas(Model model) {
+        model.addAttribute("pessoas", pessoaService.findAll());
+        model.addAttribute("pessoa", new Pessoa());
+        return "pessoas";
+    }
+
+    //Criando a API para Salvar Pessoas sem ter interface gráfica
+    @PostMapping("/pessoas")
+    public String salvarPessoa(@ModelAttribute Pessoa pessoa){
+        pessoaService.save(pessoa);
+        return "redirect:/pessoas";
+    }
+    
+
+}
