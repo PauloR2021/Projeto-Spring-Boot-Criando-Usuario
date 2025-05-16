a332e500ccb184e033a36c809022d21810978959
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-05-15 22:31:36 -0300

    Melhorando a exibição do código

--- src/main/java/com/prsoftware/project_criar_usuario/entity/Pessoa.java
+++ src/main/java/com/prsoftware/project_criar_usuario/entity/Pessoa.java
@@ -28,24 +28,28 @@ public class Pessoa {
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
     }
+    
     public String getSobrenome() {
         return sobrenome;
     }
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
+
     public String getCpf() {
         return cpf;
     }
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
     public Integer getIdade() {
         return idade;
     }
